646bb9741e4ac5e924371d572a387c820715b312
Samantha Rutledge <you@example.com>
2021-11-21 12:33:40 -0600

    Getting started!

---
+++ homework/hw_07/hw_07_functions_for_package.R
@@ -0,0 +1,11 @@
+### Functions to put in R package for Data Science Fall 2021 HW_07
+
+### Generate matrix for functions to operate on
+set.seed(123)
+x = matrix(rpois(100, 0.1), 10, 10)
+
+### Write function to remove all rows in matrix w/ 0's
+rm_0s_by_row()
+
+### Write function to remove all columns in matrix w/ 0's
+rm_0s_by_col()
