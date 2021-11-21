ab2d63d50d52866ffde93eb80891e10d5ebb617b
Samantha Rutledge <you@example.com>
2021-11-21 13:24:00 -0600

    Functions written

--- homework/hw_07/hw_07_functions_for_package.R
+++ homework/hw_07/hw_07_functions_for_package.R
@@ -5,7 +5,10 @@ set.seed(123)
 x = matrix(rpois(100, 0.1), 10, 10)
 
 ### Write function to remove all rows in matrix w/ 0's
-rm_0s_by_row()
+rm_0s_by_row <- function(x){x[rowSums(x[])>0,]}
+rm_0s_by_row(x)
+
 
 ### Write function to remove all columns in matrix w/ 0's
-rm_0s_by_col()
+rm_0s_by_col <- function(x){x[,colSums(x)!=0]}
+rm_0s_by_col(x)
